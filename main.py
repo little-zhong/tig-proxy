7550358b6df4f6eb660d6cda70c7e549c5cac935
raven <191553229@qq.com>
2024-09-07 00:11:41 +0800

    Improve logging and add headers to proxy request

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ def proxy_request(endpoint):
             url=api_url,
             params=request.args if request.method == "GET" else None,
             json=request.json if request.method == "POST" else None,
-            headers=request.headers,
+            headers=dict(request.headers),
             impersonate="chrome",
         )
 
